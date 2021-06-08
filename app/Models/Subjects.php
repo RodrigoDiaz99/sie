4092f7d508b60711dfa353da60ff646343dc7c87
KennAyala <kennayala@outlook.com>
2021-06-07 20:35:26 -0500

    update

--- app/Models/Subjects.php
+++ app/Models/Subjects.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class Subjects extends Model
 {
     use HasFactory;
+
+    public function Score()
+    {
+        return $this->belongsToMany(Score::class);
+    }
 }
